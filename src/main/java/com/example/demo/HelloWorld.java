39c46025656e91341b9eb57fac18769da2f1bc05
郑小辉 <1094121819@qq.om>
2021-01-26 17:39:47 +0800

    fix:logback 配置

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Api("helloWorld API")
 public class HelloWorld {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 
     @GetMapping("/hello")
     @ApiOperation("hello world demo")
     public String hello(@RequestParam(value = "name", defaultValue = "World") @ApiParam("输入名称，默认是 World") String name) {
+        logger.info(String.format("Hello %s!", name));
         return String.format("Hello %s!", name);
     }
 }
--- src/main/java/com/example/demo/controller/generate/UcUserController.java
+++ src/main/java/com/example/demo/controller/generate/UcUserController.java
@@ -4,12 +4,18 @@ package com.example.demo.controller.generate;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.demo.entity.UcUser;
 import com.example.demo.service.IUcUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -21,13 +27,17 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 @RequestMapping("/uc-user")
+@Api("user API")
 public class UcUserController {
 
     @Autowired
     private IUcUserService iUcUserService;
 
     @GetMapping("/testHello")
-    public UcUser testHello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return iUcUserService.getOne(new QueryWrapper<UcUser>().lambda().eq(UcUser::getNickName, name));
+    @ApiOperation("获取用户")
+    @ResponseBody
+    public UcUser testHello(@RequestParam(value = "name", defaultValue = "World") @ApiParam("用户名称") String name) {
+        UcUser user = iUcUserService.getOne(new QueryWrapper<UcUser>().lambda().eq(UcUser::getNickName, name));
+        return user;
     }
 }
